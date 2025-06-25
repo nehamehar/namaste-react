4a5612eba03996a03885ca534af44282bedd135c
Neha Mehar <nehamehar31@gmail.com>
2025-06-25 22:43:35 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+// let x = React.createElement("h1", {id:"heading"}, "hellooooo")
+//     let root = ReactDOM.createRoot(document.getElementById("root"))
+//     root.render(x);
+
+
+
+//props:
+// {id:"heading"},
+//child:
+//"hellooooo"
+
+
+// this is nested loop in js to create this in our react 
+{/* <div id="parent">
+    <div id="child">
+        <h1></h1> //two siblings
+        <h2></h2>
+    </div>
+</div> */}
+
+//********** */
+//nested loop in react
+// let x = React.createElement("div",{id:"parent"},
+//     React.createElement("div",{id:"child"},
+//         [React.createElement("h1",{},"hii there i am here"), 
+//         React.createElement("h5",{},"hii there i am here")] //2 siblings
+//     )
+// )
+
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(x);
+
+//2) how to create this  */
+{/* <div id="parent">
+    <div id="child">
+        <h1></h1> //two siblings
+        <h2></h2>
+    </div>
+</div> 
+    <div id="child2">
+        <h1></h1> //two siblings
+        <h2></h2>
+    </div>*/}
+
+
+
+//********** */
+//nested loop in react
+let x = React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"hii there i am here"), 
+        React.createElement("h5",{},"hii there i am here")], //2 siblings
+    ),
+    React.createElement("div",{id:"child2"},
+        [React.createElement("h1",{},"hii there i am here"), 
+        React.createElement("h5",{},"hii there i am here")] //2 siblings
+    )
+)
+//now its looks ugly so that why to make it easy we use jsx as react can be written with js but to make it easy we use jsx
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(x);
