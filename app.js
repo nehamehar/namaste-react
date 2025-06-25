e711b1d4c0be1e29fb2667e938c47caf4bfe564f
Neha Mehar <nehamehar31@gmail.com>
2025-06-25 23:02:06 +0530

    added ch1

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ let x = React.createElement("div",{id:"parent"},
         React.createElement("h5",{},"hii there i am here")] //2 siblings
     )
 )
-//now its looks ugly so that why to make it easy we use jsx as react can be written with js but to make it easy we use jsx
+//now its looks ugly so that why to make it easy we use jsx as react can be written with js but to make it easy we use jsx for not write these stuffs
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(x);
