7b4a44ed43f87367dbc3aab6420329cdd6356840
Neha Mehar <nehamehar31@gmail.com>
2025-06-26 19:44:06 +0530

    ch-2

---
+++ README.md
@@ -0,0 +1,20 @@
+# Namaste React🚀
+
+# Parcel:
+- Dev build 
+- Local server
+- HMR (hot module refeshment)
+- File watching algorithms - written in C++
+- Caching - Faster Builds
+- Image optimization
+- Minification 
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support all browser (when app hoisted it can open in every browser (internet explore, Chrome, Edge, Firefox, etc.))
+-  Diagnostic
+- Error Handling
+- HTTPs 
+- Tree Shaking - remove unused code
+- Different dev and prod bundler
--- app.js
+++ app.js
@@ -1,55 +1,11 @@
-// let x = React.createElement("h1", {id:"heading"}, "hellooooo")
-//     let root = ReactDOM.createRoot(document.getElementById("root"))
-//     root.render(x);
-
-
-
-//props:
-// {id:"heading"},
-//child:
-//"hellooooo"
-
-
-// this is nested loop in js to create this in our react 
-{/* <div id="parent">
-    <div id="child">
-        <h1></h1> //two siblings
-        <h2></h2>
-    </div>
-</div> */}
-
-//********** */
-//nested loop in react
-// let x = React.createElement("div",{id:"parent"},
-//     React.createElement("div",{id:"child"},
-//         [React.createElement("h1",{},"hii there i am here"), 
-//         React.createElement("h5",{},"hii there i am here")] //2 siblings
-//     )
-// )
-
-
-// const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(x);
-
-//2) how to create this  */
-{/* <div id="parent">
-    <div id="child">
-        <h1></h1> //two siblings
-        <h2></h2>
-    </div>
-</div> 
-    <div id="child2">
-        <h1></h1> //two siblings
-        <h2></h2>
-    </div>*/}
-
-
+import React from "react"
+import ReactDOM from "react-dom/client"
 
 //********** */
 //nested loop in react
 let x = React.createElement("div",{id:"parent"},
     React.createElement("div",{id:"child"},
-        [React.createElement("h1",{},"hii there i am here"), 
+        [React.createElement("h1",{},"hii there i am here.   "), 
         React.createElement("h5",{},"hii there i am here")], //2 siblings
     ),
     React.createElement("div",{id:"child2"},
@@ -61,3 +17,4 @@ let x = React.createElement("div",{id:"parent"},
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(x);
+
